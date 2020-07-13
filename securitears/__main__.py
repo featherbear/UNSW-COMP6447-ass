442c21b4daf1d80f309fef7497f3c01b62a92ee3
Andrew Wong <andrew.j.wong@outlook.com>
2020-07-14 00:52:09 +1000

    Add stub bootstrap selection

--- securitears/__main__.py
+++ securitears/__main__.py
@@ -1,19 +1,30 @@
 # Module entry-point
 
-# TODO: Turn the module into a self-contained package
 
 if __name__ == "__main__":
-    import argparse, sys
+    import argparse, sys, os.path
 
     parser = argparse.ArgumentParser(description='COMP6447 Fuzzer Project', prog="securitears", epilog="Dedicated to Adam Banana")
-    parser.add_argument('program', type=argparse.FileType('r'), help='executable file to fuzz')
+    parser.add_argument('program', type=lambda file: file if os.path.exists(file) else parser.error(f"The program {file} does not exist!"), help='executable file to fuzz')
     parser.add_argument('--input', '-i', metavar="input", type=argparse.FileType('r'), help='input file')
-    parser.add_argument('--type', '-t', choices=('json', 'csv', 'xml', 'plain', 'auto'), help='fuzz input type')
+    parser.add_argument('--type', '-t', choices=('json', 'csv', 'xml', 'plain'), help='fuzz input type')
     parser.add_argument('--dest', '-d', metavar="dest", type=argparse.FileType('w'), help='destination crash data')
     parser.add_argument('--verbose', '-v', action="store_true", help='verbose')
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
+
     args = parser.parse_args()
-    print(args)
+    
+    if args.type is None:
+        from . import detectFormat
+        bootstrap = detectFormat(args.program)
+    else:
+        from . import bootstrap
+        bootstrap = bootstrap[args.type]
+
+    print(f"Selected {bootstrap} for {args.program}")
+
+    with bootstrap(args.program) as w:
+        print(w)
