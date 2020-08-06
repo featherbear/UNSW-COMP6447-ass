fec73b1214bbeeba6bb364c478986600810b491f
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-06 12:05:28 +1000

    winky winky

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div className='content'>
+        <h1>Welcome Aboard!</h1>
+      </div>
+    );
+  }
+}
